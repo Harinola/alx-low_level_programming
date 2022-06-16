1a7d3dd64f57b3a30c620b2c9730fad837f3b260
Harinola <ayomibitoye400@gmail.com>
2022-06-16 05:03:18 -0700

    Low level programming on pointers, arrays and strings

---
+++ 0x06-pointers_arrays_starings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - joins two strings together.
+ * @dest: the destination string.
+ * @src: the source string
+ *
+ * Return: pointer to dest.
+ */
+char *_strcat(char *dest, char *src) 
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (dest);
+}
