94e7015ad02a1e1474f4fe1f6baca25a96ad85b8
Harinola <ayomibitoye400@gmail.com>
2022-06-17 10:25:40 -0700

    Low level programming on pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,32 +4,23 @@
  * @s1: the first string.
  * @s2: the second string.
  *
- * Return: 0 if the strings are equal or 15 / -15 if the strings are not equal.
- * 15 if s1 >= s2, -15 is s1 < s2 (in terms of characters).
+ * Return: 0 if the strings are equal or difference between the ASCII values of
+ * the first different characters.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int res = 0, i = 0;
+	int res = 0;
 
 	while (*s1 != '\0')
 	{
-		if (*s2 == '\0')
-		{
-			res = -15;
-			break;
-		}
 		if (*s1 != *s2)
 		{
-			i++;
+			res = *s1 - *s2;
+			break;
 		}
 		s1++;
 		s2++;
 	}
-
-	if (*s1 == *s2 && i > 0)
-		res = -15;
-	else if (*s1 != *s2 && i > 0)
-		res = 15;
 
 	return (res);
 }
