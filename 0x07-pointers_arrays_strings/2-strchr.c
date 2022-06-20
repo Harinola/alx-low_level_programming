27b35310d0e6b2107801a1c215c93cd07ee5512d
Harinola <ayomibitoye400@gmail.com>
2022-06-20 12:38:54 -0700

    A function that locates the first appearance of a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr -locates a given character in a string.
+ * @s: the address of the given string.
+ * @c: the character to be located.
+ *
+ * Return: pointer to the character if found, and NULL if not found.
+ */
+char *_strchr(char *s, char c)
+{
+	char _null = '\0', *a = _null;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			a = s;
+			break;
+		}
+		s++;
+	}
+	return (a);
+}
