d264694b5d4b3514f22dd9fa8a4ded075db5a92c
Harinola <74996966+Harinola@users.noreply.github.com>
2022-06-26 13:40:27 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,46 +7,16 @@
  */
 void print_number(int n)
 {
-	int count, num, i, indx, k, plc_hld;
+	int num;
 
-	count = 0;
-	plc_hld = 1;
-	
-	if (n == 0)
-	{
-		_putchar('0');
-	}
+	if (n < 0)
+		num = -n;
 	else
-	{
-		if (n < 0)
-		{
-			n = -n;
-			_putchar('-');
-		}
-		num = n;
-		while (num > 0)
-		{
-			count++;
-			num /= 10;
-		}
+		num = n
 
-		for (i = count; i > 0; i--)
-		{
-			indx = i - 1;
-			if (indx > 0)
-			{
-				while (indx-- > 0)
-				{
-					plc_hld *= 10;
-				}
-				k = (n / plc_hld) % 10;
-			}
-			else
-			{
-				k = n % 10;
-			}
-			_putchar((k) + '0');
-		}
-	}
+	if (num / 10)
+		print_number(num / 10);
+	
+	_putchar((num % 10) + '0');
 
 }
