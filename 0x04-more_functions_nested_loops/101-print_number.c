abce3bbc87f6eae978209d4bd859337af5fbcae3
Harinola <74996966+Harinola@users.noreply.github.com>
2022-06-26 13:45:54 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,13 +10,20 @@ void print_number(int n)
 	int num;
 
 	if (n < 0)
+	{
+		_putchar('-');
 		num = -n;
+	}
 	else
+	{
 		num = n;
+	}
 
 	if (num / 10)
+	{
 		print_number(num / 10);
-	
+	}
+
 	_putchar((num % 10) + '0');
 
 }
