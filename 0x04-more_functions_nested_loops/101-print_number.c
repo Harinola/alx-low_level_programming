a21b127ba3629c8b353232340d835ab79324af1b
Harinola <ayomibitoye400@gmail.com>
2022-06-26 04:48:45 -0700

    Added last advance project and updated header file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - prints a number
+ * @n: number to be printed
+ *
+ * Return: void.
+ */
+void print_number(int n)
+{
+	int count, num, i, k;
+
+	count = 0;
+	num = n;
+
+	while (num > 0)
+	{
+		count++;
+
+		num /= 10;
+	}
+	if (n < 0)
+	{
+		n = -n;
+
+		_putchar('-');
+	}
+	for (i = count; i > 0; i--)
+	{
+/*		n %= 10;*/
+/*		k = n;*/
+		_putchar((n %= 10) + '0');
+	}
+
+	_putchar('\n');
+}
