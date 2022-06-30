4d60a13fb2ebdc6fb5ba6f2b94a170b10b972c08
Harinola <ayomibitoye400@gmail.com>
2022-06-30 13:54:02 -0700

    syntax correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,14 +1,30 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-unsigned int _strlen(char *s);
 /**
- * string_nconcat - Concatenates n bytes of s2 unto s1.
+ * _strlen - Checks the length of a string.
+ * @s: The given string.
+ *
+ * Return: Length of the string.
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int cnt;
+
+	cnt = 0;
+
+	if (s == NULL)
+		return (cnt);
+	while (s[cnt])
+		cnt++;
+
+	return (cnt);
+}
+/**
+ * string_nconcat - Concatenates n bytes of s2 unto s1 in a new memory.
  * @s1: First string.
  * @s2: Second string.
- * @n: Number of bytes.
  *
- * Return: pointer to the gotten string(Success), NULL(Failure).
+ * Return: Address of the new memory(Success), NULL(Failure).
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -22,39 +38,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (len > _strlen(s2))
 		len = _strlen(s2);
 
-	s = malloc(sizeof(char) * (_strlen(s1) + len + 1));
-	if (s = NULL)
+	s = malloc(sizeof(*s) * (_strlen(s1) + len + 1));
+	if (s == NULL)
 		return (NULL);
 
-	while (s1[i] !='\0')
-	{
-		s[i++] = s1[i++];
-	}
+	while (i < _strlen(s1))
+		s[i++] = s1[i];
 
 	while (j < len)
-	{
 		s[i++] = s2[j++];
-	}
 
 	s[i] = '\0';
-	printf("%u\n", len);
 
 	return (s);
-}
-
-/**
- * _strlen - Checks the length of a string.
- * @s: the given string.
- *
- * Return: Length of the string.
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int cnt;
-
-	cnt = 0;
-	while (s[cnt])
-		cnt++;
-
-	return (cnt);
 }
