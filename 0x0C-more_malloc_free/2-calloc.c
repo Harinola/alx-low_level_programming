81db18ce9563673dff3e5f16ad8af97280d9156a
Harinola <ayomibitoye400@gmail.com>
2022-06-30 18:48:34 -0700

    Task 2 correction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -33,7 +33,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
-	if (size <= 0 || nmemb <= 0)
+	if (size == 0 || nmemb == 0)
 		return (NULL);
 
 	ptr = malloc(size * nmemb);
